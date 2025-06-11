1b186d8f9f1cc4daf72cad0a9f0f1327a0d20321
ducgym05 <danggiaduc0905@gmail.com>
2025-06-11 10:46:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from members.hello_Cong import hello_cong
 from members.hello_Nguyen import hello_Nguyen
 from members.hellohoangduy import hello_duy
+from members.hello_quy import hello_quy
 
 def main():
   hello_cong()
   hello_Nguyen()
   hello_duy()
+  hello_quy()
   
 if __name__ == "__main__":
     main()
