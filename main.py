3cebb54dc8394c7f5790b59b949a49c54e9ae5ae
ducgym05 <danggiaduc0905@gmail.com>
2025-06-12 10:04:29 +0700

    AI

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from members.hello_Cong import hello_cong
 from members.hello_Nguyen import hello_Nguyen
 from members.hellohoangduy import hello_duy
 from members.hello_quy import hello_quy
+from members.hello_duc import hello_duc
 
 def main():
   hello_cong()
   hello_Nguyen()
   hello_duy()
   hello_quy()
-  
+  hello_duc()
+
 if __name__ == "__main__":
     main()
